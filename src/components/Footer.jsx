537f1fd1afeeeb380a7820c3791083a50207c4e8
Jyoti988 <Yjyoti654@gmail.com>
2025-01-08 00:41:08 +0530

    admin dashboard design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 const Footer = () => {
   return (
     <footer id="footer">
@@ -7,6 +6,7 @@ const Footer = () => {
       <div className="section">
         <div className="container">
           <div className="row">
+            {/* About Us */}
             <div className="col-md-3 col-xs-6">
               <div className="footer">
                 <h3 className="footer-title">About Us</h3>
@@ -15,17 +15,17 @@ const Footer = () => {
                 </p>
                 <ul className="footer-links">
                   <li>
-                    <a href="#">
+                    <a href="#" className="footer-link">
                       <i className="fa fa-map-marker"></i>1734 Stonecoal Road
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a href="#" className="footer-link">
                       <i className="fa fa-phone"></i>+021-95-51-84
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a href="#" className="footer-link">
                       <i className="fa fa-envelope-o"></i>email@email.com
                     </a>
                   </li>
@@ -33,43 +33,44 @@ const Footer = () => {
               </div>
             </div>
 
+            {/* Categories */}
             <div className="col-md-3 col-xs-6">
               <div className="footer">
                 <h3 className="footer-title">Categories</h3>
                 <ul className="footer-links">
-                  <li><a href="#">Hot deals</a></li>
-                  <li><a href="#">Laptops</a></li>
-                  <li><a href="#">Smartphones</a></li>
-                  <li><a href="#">Cameras</a></li>
-                  <li><a href="#">Accessories</a></li>
+                  <li><a href="#" className="footer-link">Hot deals</a></li>
+                  <li><a href="#" className="footer-link">Laptops</a></li>
+                  <li><a href="#" className="footer-link">Smartphones</a></li>
+                  <li><a href="#" className="footer-link">Cameras</a></li>
+                  <li><a href="#" className="footer-link">Accessories</a></li>
                 </ul>
               </div>
             </div>
 
-            <div className="clearfix visible-xs"></div>
-
+            {/* Information */}
             <div className="col-md-3 col-xs-6">
               <div className="footer">
                 <h3 className="footer-title">Information</h3>
                 <ul className="footer-links">
-                  <li><a href="#">About Us</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Privacy Policy</a></li>
-                  <li><a href="#">Orders and Returns</a></li>
-                  <li><a href="#">Terms & Conditions</a></li>
+                  <li><a href="#" className="footer-link">About Us</a></li>
+                  <li><a href="#" className="footer-link">Contact Us</a></li>
+                  <li><a href="#" className="footer-link">Privacy Policy</a></li>
+                  <li><a href="#" className="footer-link">Orders and Returns</a></li>
+                  <li><a href="#" className="footer-link">Terms & Conditions</a></li>
                 </ul>
               </div>
             </div>
 
+            {/* Service */}
             <div className="col-md-3 col-xs-6">
               <div className="footer">
                 <h3 className="footer-title">Service</h3>
                 <ul className="footer-links">
-                  <li><a href="#">My Account</a></li>
-                  <li><a href="#">View Cart</a></li>
-                  <li><a href="#">Wishlist</a></li>
-                  <li><a href="#">Track My Order</a></li>
-                  <li><a href="#">Help</a></li>
+                  <li><a href="#" className="footer-link">My Account</a></li>
+                  <li><a href="#" className="footer-link">View Cart</a></li>
+                  <li><a href="#" className="footer-link">Wishlist</a></li>
+                  <li><a href="#" className="footer-link">Track My Order</a></li>
+                  <li><a href="#" className="footer-link">Help</a></li>
                 </ul>
               </div>
             </div>
@@ -77,31 +78,7 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Bottom Footer */}
-      <div id="bottom-footer" className="section">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 text-center">
-              <ul className="footer-payments">
-                <li><a href="#"><i className="fa fa-cc-visa"></i></a></li>
-                <li><a href="#"><i className="fa fa-credit-card"></i></a></li>
-                <li><a href="#"><i className="fa fa-cc-paypal"></i></a></li>
-                <li><a href="#"><i className="fa fa-cc-mastercard"></i></a></li>
-                <li><a href="#"><i className="fa fa-cc-discover"></i></a></li>
-                <li><a href="#"><i className="fa fa-cc-amex"></i></a></li>
-              </ul>
-              <span className="copyright">
-                Copyright &copy;
-                <script>{`document.write(new Date().getFullYear());`}</script> All rights reserved | This template is made with{' '}
-                <i className="fa fa-heart-o" aria-hidden="true"></i> by{' '}
-                <a href="https://colorlib.com" target="_blank" rel="noopener noreferrer">
-                  Colorlib
-                </a>
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      
     </footer>
   );
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,59 +10,7 @@ const Index = () => {
 
   return (
     <>
-      
-      <div className="section">
-        <div className="container">
-          <div className="row">
-            {/* Laptop Collection */}
-            <div className="col-md-4 col-xs-6">
-              <div className="shop">
-                <div className="shop-img">
-                  <img src="/assets/img/shop01.png" alt="" />
-                </div>
-                <div className="shop-body">
-                  <h3>Laptop<br />Collection</h3>
-                  <a href="#" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
-                </div>
-              </div>
-            </div>
-
-            {/* Accessories Collection */}
-            <div className="col-md-4 col-xs-6">
-              <div className="shop">
-                <div className="shop-img">
-                  <img src="./img/shop03.png" alt="" />
-                </div>
-                <div className="shop-body">
-                  <h3>Accessories<br />Collection</h3>
-                  <a href="#" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
-                </div>
-              </div>
-            </div>
-
-            {/* Camera Collection */}
-            <div className="col-md-4 col-xs-6">
-              <div className="shop">
-                <div className="shop-img">
-                  <img src="./img/shop02.png" alt="" />
-                </div>
-                <div className="shop-body">
-                  <h3>Cameras<br />Collection</h3>
-                  <a href="#" className="cta-btn">Shop now <i className="fa fa-arrow-circle-right"></i></a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-
-
-
-
-
-
-
+		
     </>
   );
 };
